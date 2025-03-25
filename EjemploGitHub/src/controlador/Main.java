b750a38ff90a4e82df8bf9bfe751dd4efe22c605
ikasle <ikasle@DAM1-13.arangoya.local>
2025-03-25 14:13:54 +0100

    version fusionada

--- EjemploGitHub/src/controlador/Main.java
+++ EjemploGitHub/src/controlador/Main.java
@@ -7,18 +7,12 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("hola dos");
 	System.out.println("Commit con pareja");
 	System.out.println("Garazi");
 	
 	System.out.println("Prueba con Merge...");
 
-=======
-	System.out.println("hola ALGO");
-	System.out.println("NEVER GONNA GIVE YOU UP");
-	
->>>>>>> branch 'main' of https://github.com/GaraziLabaka/Ejemplo.git
 	}
 
 }
